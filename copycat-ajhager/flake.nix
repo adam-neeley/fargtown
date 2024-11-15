ee1161805973bbb11a322769e594e37b027d333f
Adam Neeley <adam.t.neeley@protonmail.com>
2024-11-14 18:46:32 -0800

    run copycat-ajhager

---
+++ copycat-ajhager/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "copycat-ajhager";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+  inputs.pyproject-nix.url = "github:nix-community/pyproject.nix";
+  inputs.pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, pyproject-nix, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        project =
+          pyproject-nix.lib.project.loadPyproject { projectRoot = ./.; };
+        python = pkgs.python312;
+        arg = project.renderers.withPackages { inherit python; };
+        pythonEnv = python.withPackages arg;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs;
+            [
+              # python312Packages.pylint
+              # python312Packages.pyflakes
+            ] ++ [ pythonEnv ];
+        };
+        packages.default =
+          let attrs = project.renderers.buildPythonPackage { inherit python; };
+          in python.pkgs.buildPythonPackage attrs;
+      });
+}
